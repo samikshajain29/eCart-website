63b028c4fda73a34cf5efb23e578f9f73ce74821
Samiksha Jain <samikshaj7371@gmail.com>
2025-07-08 15:49:16 +0530

    setup cloudinary

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Nav from "../components/Nav";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
-  return <div className="text-[30px] text-[red]">home</div>;
+  return (
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] relative">
+      <Nav />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default Home;
