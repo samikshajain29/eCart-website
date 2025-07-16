11130e2347e9848f896a5e2bd9d2ef20c54f4ae6
Samiksha Jain <samikshaj7371@gmail.com>
2025-07-16 20:05:33 +0530

    Add latest collection and best seller

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Title from "./Title";
+import { shopDataContext } from "../context/ShopContext";
+import Card from "./Card";
 
 function BestSeller() {
-  return <div></div>;
+  let { products } = useContext(shopDataContext);
+  let [bestSeller, setBestSeller] = useState([]);
+
+  useEffect(() => {
+    let filterProduct = products.filter((item) => item.bestseller);
+    setBestSeller(filterProduct.slice(0, 4));
+  }, [products]);
+  return (
+    <div>
+      <div className="h-[8%] w-[100%] text-center mt-[50px]">
+        <Title text1={"BEST"} text2={"SELLER"} />
+        <p className="w-[100%] m-auto text-[13px] md:text-[20px] px-[10px] text-blue-100">
+          Tried, Tested, Loved Discover Our All-Time Best Sellers.
+        </p>
+      </div>
+      <div className="w-[100%] h-[50%] mt-[30px] flex items-center justify-center flex-wrap gap-[50px]">
+        {bestSeller.map((item, index) => (
+          <Card
+            key={index}
+            name={item.name}
+            image={item.image1}
+            id={item._id}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default BestSeller;
