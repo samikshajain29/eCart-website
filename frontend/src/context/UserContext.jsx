88fed98fba23c70fa69c273b2afd70bce11ec8e2
Samiksha Jain <samikshaj7371@gmail.com>
2025-08-03 20:57:49 +0530

    Resolve error

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useContext } from "react";
 import { useState } from "react";
 import { createContext } from "react";
-import { authDataContext } from "./authContext";
+import { authDataContext } from "./AuthContext";
 import axios from "axios";
 import { useEffect } from "react";
 
