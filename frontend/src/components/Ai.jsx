686f7ee906653d83740d48546fcd3babaa6a8d8c
Samiksha Jain <samikshaj7371@gmail.com>
2025-08-26 23:49:12 +0530

    Change ai image

--- frontend/src/components/Ai.jsx
+++ frontend/src/components/Ai.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import ai from "../assets/ai.png";
+import ai from "../assets/ai.webp";
 import { shopDataContext } from "../context/ShopContext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
